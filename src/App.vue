bbe37c4a218210edd60477a3cb1c6d3588871b6f
Felicia Brenner <felicia.brenner514@gmail.com>
2024-03-28 18:38:21 -0500

    feat: vue practice

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    data() {
+        return {
+            count: 0,
+            author: {
+                name: 'Jane Doe',
+                books: [
+                    'Vue 2 - Advanced Guide',
+                    'Vue 3 - basic guide',
+                ]
+            }
+        }
+    },
+    methods: {
+        increment() {
+            this.count++
+        }
+    },
+
+    mounted() {
+        console.log(this.count)
+        this.increment()
+    }
+}
+</script>
+<template>
+    <h1>Hello Vue</h1>
+    <p>This is my app</p>
+    <p>Count is {{ count }}</p>
+    <button @click="increment">{{ count }}</button>
+    <span>{{ author.books.length > 0 ? 'yes' : 'no' }}</span>
+</template>
